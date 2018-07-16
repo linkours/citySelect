c166b678730210218b017de754121cfccc923c27
unknown <you@example.com>
2018-07-16 16:36:16 +0800

    element ui 省市区级联选择器

---
+++ select.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-cascader
+      :options="options"
+      v-model="selectedOptions"
+      :separator="'/'">
+    </el-cascader>
+  </div>
+</template>
+
+<script>
+import options from '../../../static/test'
+export default {
+  data() {
+    return {
+      selectedOptions: [],
+      options: options
+    }
+  }
+}
+</script>
+
+
